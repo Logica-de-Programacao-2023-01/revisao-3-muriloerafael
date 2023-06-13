ce6b4914dc38aebfee2de46b6dd989a3cb7cb67d
Rafael <rafaelmarliere033@gmail.com>
2023-06-13 14:28:45 -0300

    2 solved with explication

--- q2/q2.go
+++ q2/q2.go
@@ -1,9 +1,22 @@
 package q2
 
+import "strings"
+
 func LongestCommonPrefix(strs []string) string {
-	for _, str := range strs {
-		
+	if len(strs) == 0 { // se strs = []
+		return "" // output = ""
+	}
+	// strs = ["flower","flow","flight"]
+	prefix := strs[0] // prefix = "flower"
+
+	for i := 1; i < len(strs) && len(prefix) > 0; i++ { // loop percorrendo ["flow", "flight"]
+		for !strings.HasPrefix(strs[i], prefix) {
+			// enquanto a palavra atual nao tiver o prefixo, exemplo: flow nao con
+			// tem flower, nem flowe, mas contem flow. Depois : flight nao contem
+			// flow, nem flo, mas contem fl, que eh o maior prefixo. Ele vai diminuindo na linha debaixo
+			prefix = prefix[:len(prefix)-1] // flower -> flowe -> flow, depois compara de novo: flow -> flo -> fl.
+		}
 	}
 
-	return ""
+	return prefix // output = "fl"
 }

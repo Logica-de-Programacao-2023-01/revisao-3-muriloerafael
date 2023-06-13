6151e6a170b0d406b410c535238f5c03b7a22d72
Rafael <rafaelmarliere033@gmail.com>
2023-06-13 19:53:51 -0300

    question 5

--- q5/q5.go
+++ q5/q5.go
@@ -1,11 +1,29 @@
 package q5
 
-//Uma frase é um palíndromo se, após converter todas as letras maiúsculas em letras minúsculas e remover todos os
-//caracteres não alfanuméricos, ela for lida da mesma forma da esquerda para a direita e vice-versa. Caracteres
-//alfanuméricos incluem letras e números.
-//
-//Dada uma string "s", retorne verdadeiro se for um palíndromo e falso caso contrário.
+import "strings"
+
+func reverse(str string) string {
+	runes := []rune(str)
+
+	for i, j := 0, len(runes)-1; i < len(runes)/2; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+
+	return string(runes)
+}
 
 func IsPalindrome(s string) bool {
+	s = strings.ToLower(s)
+
+	specialChars := []string{":", ",", " "}
+
+	for _, char := range specialChars {
+		s = strings.ReplaceAll(s, char, "")
+	}
+
+	if reverse(s) == s {
+		return true
+	}
+
 	return false
 }

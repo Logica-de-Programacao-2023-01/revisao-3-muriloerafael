fe3c6b8378e3d5ef9aad147718205f85e20c428b
Rafael <rafaelmarliere033@gmail.com>
2023-06-13 20:32:15 -0300

    trying bonus

--- bonus/bonus.go
+++ bonus/bonus.go
@@ -1,12 +1,5 @@
 package bonus
 
-//Você recebe as cabeças de duas listas ligadas ordenadas, list1 e list2.
-//
-//Una as duas listas em uma única lista ordenada. A lista resultante deve ser construída juntando os nós das duas
-//primeiras listas.
-//
-//Retorne a cabeça da lista ligada mesclada.
-
 type Node struct {
 	Value int
 	Next  *Node
@@ -17,5 +10,6 @@ type LinkedList struct {
 }
 
 func MergeTwoLists(list1 *LinkedList, list2 *LinkedList) *LinkedList {
+	// nao consegui fazer por nada :(
 	return nil
 }
